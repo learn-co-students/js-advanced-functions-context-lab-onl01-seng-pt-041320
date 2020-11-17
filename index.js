13c6a851f6091b0fd3716db39614786f28ab4a6a
baker615512 <baker615512@gmail.com>
2020-11-17 14:02:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 /* Your Code Here */
 
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(array){
+    return array.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateStamp){
+    let date = dateStamp.split(" ")[0]
+    let hour = parseInt(dateStamp.split(" ")[1])
+    this.timeInEvents.push({type: "TimeIn", date: date, hour: hour})
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    let date = dateStamp.split(" ")[0]
+    let hour = parseInt(dateStamp.split(" ")[1])
+    this.timeOutEvents.push({type: "TimeOut", date: date, hour: hour})
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp){
+    let date = dateStamp.split(" ")[0]
+    let inEvent = this.timeInEvents.find((e) => {return e.date === date})
+    let outEvent = this.timeOutEvents.find((e) => {return e.date === date})
+    let hours = parseInt(outEvent.hour) - parseInt(inEvent.hour)
+    return hours / 100
+}
+
+function wagesEarnedOnDate(date){
+    let payRate = this.payPerHour
+    let hours = hoursWorkedOnDate.call(this, date)
+    return payRate * hours
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,4 +61,14 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find((employee) => employee.firstName = firstName)
+}
+
+function calculatePayroll(srcArray){
+    return srcArray.reduce((payroll, employees) => {
+      return payroll + allWagesFor.call(employees)
+    }, 0)
 }
